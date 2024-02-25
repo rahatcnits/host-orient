7e1b3ff53e1f4630a99e51c9613099717050fc33
rahatcnits <rahatcnits@gmail.com>
2024-02-26 00:11:50 +0600

    email server page started

---
+++ assets/js/accordion.js
@@ -0,0 +1,34 @@
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
+const accordionBoxs = document.querySelectorAll(".accordion-item");
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+
+    const currentlyActiveAccordionItemHeader = document.querySelector(
+      ".accordion-item-header.active"
+    );
+    if (
+      currentlyActiveAccordionItemHeader &&
+      currentlyActiveAccordionItemHeader !== accordionItemHeader
+    ) {
+      currentlyActiveAccordionItemHeader.classList.toggle("active");
+      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+    }
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    } else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemHeader.parentElement.classList.add("active");
+    } else {
+      accordionItemHeader.parentElement.classList.remove("active");
+    }
+  });
+});
