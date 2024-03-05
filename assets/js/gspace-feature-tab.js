2b92424dcb43e25b594bfd49cfaf2f52dd3f1def
rahatcnits <rahatcnits@gmail.com>
2024-03-05 21:06:36 +0600

    google space features ongoing

---
+++ assets/js/gspace-feature-tab.js
@@ -0,0 +1,25 @@
+// tabs
+
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+tabLinks.forEach(function (el) {
+  el.addEventListener("click", openTabs);
+});
+
+function openTabs(el) {
+  var btnTarget = el.currentTarget;
+  var tabs = btnTarget.dataset.tabs;
+
+  tabContent.forEach(function (el) {
+    el.classList.remove("active");
+  });
+
+  tabLinks.forEach(function (el) {
+    el.classList.remove("active");
+  });
+
+  document.querySelector("#" + tabs).classList.add("active");
+
+  btnTarget.classList.add("active");
+}
