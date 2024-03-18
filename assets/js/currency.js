dac8281eb5ff88c527a3437347313b024afa3e8a
rahatcnits <rahatcnits@gmail.com>
2024-03-19 04:13:12 +0600

    terms service ongoing

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -2,7 +2,7 @@
 const exchangeRate = 100; // 1 USD = 100 BDT
 
 function changeCurrency() {
-  const selectedCurrency = document.getElementById("currencyToggle").value;
+  const selectedCurrency = document.querySelector(".currencyToggle").value;
   console.log(selectedCurrency);
   const products = document.querySelectorAll(".product");
 
@@ -11,11 +11,36 @@ function changeCurrency() {
     const priceInBDT = priceInUSD * exchangeRate; // Convert USD to BDT using the fixed rate
 
     const convertedPrice = selectedCurrency === "BDT" ? priceInBDT : priceInUSD;
+    const currencyIcon = selectedCurrency === "BDT" ? "৳" : "$";
     const displayPrice = convertedPrice.toFixed(2); // Optional: format to 2 decimal places
 
-    product.querySelector(".price").innerText = `${displayPrice}`;
+    product.querySelector(
+      ".price"
+    ).innerText = `${currencyIcon}${displayPrice}`;
   });
 }
 
 // Initial conversion to reflect the default selected currency on page load
 document.addEventListener("DOMContentLoaded", changeCurrency);
+
+function changeCurrencyPhn() {
+  const selectedCurrency = document.querySelector(".currencyTogglePhn").value;
+  console.log(selectedCurrency);
+  const products = document.querySelectorAll(".product");
+
+  products.forEach((product) => {
+    const priceInUSD = parseFloat(product.getAttribute("data-price-usd"));
+    const priceInBDT = priceInUSD * exchangeRate; // Convert USD to BDT using the fixed rate
+
+    const convertedPrice = selectedCurrency === "BDT" ? priceInBDT : priceInUSD;
+    const currencyIcon = selectedCurrency === "BDT" ? "৳" : "$";
+    const displayPrice = convertedPrice.toFixed(2); // Optional: format to 2 decimal places
+
+    product.querySelector(
+      ".price"
+    ).innerText = `${currencyIcon}${displayPrice}`;
+  });
+}
+
+// Initial conversion to reflect the default selected currency on page load
+document.addEventListener("DOMContentLoaded", changeCurrencyPhn);
