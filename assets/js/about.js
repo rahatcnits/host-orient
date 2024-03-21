409b642140fa1596b94546151b721cba18eda2b0
rahatcnits <rahatcnits@gmail.com>
2024-03-21 16:40:49 +0600

    about page ongoing

---
+++ assets/js/about.js
@@ -0,0 +1,41 @@
+// Owner and Founder
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+// Roadmap
+var swiper = new Swiper(".roadmapSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Partners
+$("#sliderPartner").owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: false,
+  dots: false,
+  slideBy: 1,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 4,
+    },
+  },
+});
